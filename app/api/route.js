521061b37a932a605f24c2d08ab0d27792dea13c
harish-pandeyy <hpandeyy97088@gmail.com>
2024-12-18 17:19:59 +0530

    first commit

--- app/api/route.js
+++
@@ -1,7 +0,0 @@
-const get = (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ message: 'Hello from the Daily route' }))
-}
-
-export default get;
